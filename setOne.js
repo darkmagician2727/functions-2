1591830ca85db3a07bb4ce688f6c73cfa1f0d55e
Thomas Albrecht <darkmagician2727@gmail.com>
2023-09-20 15:47:14 -0600

    commit2

--- setOne.js
+++ setOne.js
@@ -11,39 +11,81 @@ function findGrape (arr) {
 // ***** Do not edit the code above *****
 
 /*
-The code above is an example of 'function declaration.' Please re-write the function in 'function expression' and 'arrow function' syntaxes. 
+The code above is an example of 'function declaration.' Please re-write the function in 'function expression' 
+and 'arrow function' syntaxes. 
 */
 
 // RE-WRITE THE ABOVE FUNCTION IN 'FUNCTION EXPRESSION' SYNTAX HERE.
 
-
+const expressionfindGrape = function(arr){
+    for(let i=0; i<arr.length; i++) {
+        if(arr[i].color === "purple") {
+            return console.log(`The fruit with index ${arr.indexOf(arr[i])} is a grape`)
+        } 
+    }
+}
 
 // RE-WRITE THE ABOVE FUNCTION IN 'ARROW FUNCTION' SYNTAX HERE.
 
-
+const ArrowfindGrape = (arr) => {
+    for(let i=0; i<arr.length; i++) {
+        if(arr[i].color === "purple") {
+            return console.log(`The fruit with index ${arr.indexOf(arr[i])} is a grape`)
+        } 
+    }
+}
 
 ////////// PROBLEM 2 //////////
 /*
-Write a one line function (give a name of your choice) with an implicit return that when the function is invoked, it will print: 'The first parameter is [parameter1]. The second parameter is [parameter2]'.
+Write a one line function (give a name of your choice) with an implicit return that when the function is invoked, 
+it will print: 'The first parameter is [parameter1]. The second parameter is [parameter2]'.
 */
 
 // CODE HERE
 
+const parameterfunction = (param1, param2) => {console.log(`The first parameter is ${param1}. The second parameter is ${param2} `)}
+
 
 // INVOKE THE FUNCTION HERE. THE PARAMETERS TAKE ANY DATATYPE.
+
+parameterfunction(1, "string")
 
 
 ////////// PROBLEM 3 //////////
 /*
-Write a function called 'greeting' that takes 3 parameters: firstName, lastName, and a callback function. In the function, invoke the callback, passing in a concatenation of firstName and lastName. 
-Then, outside of the greeting function, invoke the greeting function, passing in the value firstName and lastName of your choice, and a callback function that takes a parameter of fullName. The callback function will print 'Hello, my full name is [fullName]'. 
+Write a function called 'greeting' that takes 3 parameters: firstName, lastName, and a callback function. In the function, 
+invoke the callback, passing in a concatenation of firstName and lastName. Then, outside of the greeting function, 
+invoke the greeting function, passing in the value firstName and lastName of your choice, 
+and a callback function that takes a parameter of fullName. 
+The callback function will print 'Hello, my full name is [fullName]'. 
 */
 
 // CODE 'GREETING FUNCTION' HERE
 
+// function greeting(firstName, lastName){
+//     let fullName = firstName + ` ` + lastName
+//     return fullName
+// }
+
+// callback2(greeting(tj, albrecht))
+
+
+function greeting(firstName, lastName, callb){
+    let fullName = firstName + ` ` + lastName
+    callb(fullName)
+}
+
+function callback1(fullName){
+    console.log(`Hello, my full name is ${fullName}`)
+}
+
+// function callback2(fullName){
+//     console.log(`No, my full name is ${fullName}`)
+// }
 
 // INVOKE 'GREETING FUNCTION' HERE
 
+greeting("Dwayne", "The rock", callback1)
 
 ////////// PROBLEM 4 //////////
 
@@ -54,41 +96,60 @@ let totalCost = []
 // ***** Do not edit the code above *****
 
 /* 
-Write a function called 'pricesPlusTax' that takes 2 params: an array ('prices' array) and a callback function. Inside the function, loop over the 'prices' array. For every iteration, calculate a new total cost (original price plus 20% tax), then push it to the 'totalCost' array. After the loop, invoke the callback function, passing in the 'totalCost' array.
+Write a function called 'pricesPlusTax' that takes 2 params: an array ('prices' array) and a callback function. 
+Inside the function, loop over the 'prices' array. For every iteration, 
+calculate a new total cost (original price plus 20% tax), then push it to the 'totalCost' array. After the loop, 
+invoke the callback function, passing in the 'totalCost' array.
 */
 
 // CODE HERE
 
+function pricesPlusTax(arr, cb){
+    for(let i=0; i<arr.length; i++) {
+        totalCost.push(arr[i]*1.2)
+    }
+    cb(totalCost)
+}
 
 /* 
-Invoke the 'pricesPlusTax' function, passing in the 'prices' array and a callback function (passing in 'totalCost' as a param) that will print "The new array plus tax = [totalCost]"
+Invoke the 'pricesPlusTax' function, passing in the 'prices' array 
+and a callback function (passing in 'totalCost' as a param) that will print "The new array plus tax = [totalCost]"
 */
 
 // CODE HERE
 
+function NewPricePrint(arr){
+    console.log(`The new array plus tax = ${arr}`)
+}
+
+pricesPlusTax(prices, NewPricePrint)
 
 ////////// PROBLEM 5 //////////
 
 /* 
 A function can return another function. Let's create one!
 
-Create a function called 'multiplyingFactory' that takes a number as a param. The function returns another function that takes another number as a param. 
+Create a function called 'multiplyingFactory' that takes a number as a param. 
+The function returns another function that takes another number as a param. 
 
-The inner function should run this logic: if the first number passing in is greater than and equal to 5, print the multiplication of the first and second numbers. If not, print "Cannot multiply: the first number is smaller than 5." 
+The inner function should run this logic: if the first number passing in is greater than and equal to 5, 
+print the multiplication of the first and second numbers. If not, print "Cannot multiply: the first number is smaller than 5." 
 */
 
 // CODE HERE
 
 
 /* 
-Let's invoke the 'multiplyingFactory' function that will return another function, and save it into a variable called 'timesFour.' Let's pass in number 3 as a param.
+Let's invoke the 'multiplyingFactory' function that will return another function, 
+and save it into a variable called 'timesFour.' Let's pass in number 3 as a param.
 */
 
 // CODE HERE
 
 
 /* 
-Now, timesFour is the new function (the inner function that was being returned when we invoked 'multiplyingFactory' function). The number 3 that we passed in as a first number is now saved in the 'timesFour' function. 
+Now, timesFour is the new function (the inner function that was being returned when we invoked 'multiplyingFactory' function). 
+The number 3 that we passed in as a first number is now saved in the 'timesFour' function. 
 
 Let's invoke 'timesFour' and pass in number 4 as a param. Number 4 here is the second number that will multiply the first number (number 3).
 
